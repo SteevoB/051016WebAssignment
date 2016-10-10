1fdd2ac92a6b1adb5fe9cb7028cefd9494431fc3
Steven Bergqvist <stevenbergqvist@gmail.com>
2016-10-11 00:27:58 +0200

    PC switch

---
+++ 051016WebAssignment/Experience.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _051016WebAssignment
+{
+    public class Experience
+    {
+        public Experience()
+        {
+
+        }
+
+        public string Education { get; set; }
+        public string Experience { get; set; }
+        public string Period get; set; }
+    }
+}
